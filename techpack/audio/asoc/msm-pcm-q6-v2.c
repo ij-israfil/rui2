9848bc6631dee0fbe8bec163905d7043c4750c76
Soumya Managoli <quic_c_smanag@quicinc.com>
2023-01-06 14:37:20 +0530

    ASoC: msm-pcm-q6-v2: Add dsp buf check
    Current logic copies user buf size of data
    from the avail dsp buf at a given offset.
    If this offset returned from DSP in READ_DONE event
    goes out of bounds or is corrupted, then it can lead to
    out of bounds DSP buffer access, resulting in memory fault.
    Fix is to add check for this buf offset, if it is within
    the buf size range.
    
    Change-Id: I7753cc6db394704dbb959477150141d42b836bef
    Signed-off-by: Soumya Managoli <quic_c_smanag@quicinc.com>
    Signed-off-by: Ij Israfil <israfilkhan494@gmail.com>

--- techpack/audio/asoc/msm-pcm-q6-v2.c
+++ techpack/audio/asoc/msm-pcm-q6-v2.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2020, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2019, The Linux Foundation. All rights reserved.
  * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
